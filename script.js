1137473833bcfdaec68308ccfbec23f5de84f531
sofiulaloe <sahasofiulalom3@gmail.com>
2022-09-17 05:01:22 -0700

    show-time in  parseint add  commit

--- script.js
+++ script.js
@@ -138,5 +138,5 @@ setInterval(() => {
   const currentTime =new Date().getTime() 
   const timeSpent = (currentTime- startTime) / 1000;
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
